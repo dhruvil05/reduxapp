98f767004be4b43156f835c182b573e3e29d8813
Your Name <dhruvil@phpdots.com>
2023-10-04 20:58:21 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { useSelector, useDispatch} from "react-redux";
+import {incNumber, decNumber} from "./actions/index"
+const App = () => {
+    const myState = useSelector((state) => state.changeTheNumber);
+    const dispatch = useDispatch();
+    return (
+        <>
+            <div className="container">
+                <h1>Increment/Decrement counter</h1>
+                <h4>using React and Redux</h4>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                <div className="quantity">
+                    <a title="Decrement" onClick={()=>dispatch(decNumber())} className="quantity_minus"><span> - </span></a>
+                    <input type="text" name="quantity" className="quantity_input" value={myState} />
+                    <a title="Increment" onClick={()=>dispatch(incNumber())} className="quantity_plus"><span> + </span></a>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
